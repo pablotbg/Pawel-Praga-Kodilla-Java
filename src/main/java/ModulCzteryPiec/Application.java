7fb0a507085f846ad7bccaa5f545f128ab09a4c0
Paweł Praga <p.praga@o2.pl>
2020-09-27 18:00:22 +0200

    First Commit

--- src/main/java/ModulCzteryPiec/Application.java
+++ src/main/java/ModulCzteryPiec/Application.java
@@ -1,20 +1,57 @@
 package ModulCzteryPiec;
 
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 class Application {
     public static void main (String[] args) {
         class Clock {
+            String clock;
+            LocalTime time;
+
             public Clock() {
+                //this.time = this.time.plusMinutes(1);
+            }
+
+            public Clock(String clock, int hour, int minute) {
+                this.clock = clock;
+                this.time = LocalTime.of(hour, minute).plusMinutes(1);
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                Clock clocks = (Clock) o;
+                return Objects.equals(clock, clocks.clock) &&
+                        Objects.equals(time.getHour(), clocks.getTime().getHour()) &&
+                        Objects.equals(time.getMinute(), clocks.getTime().getMinute());
+            }
+
+            @Override
+            public int hashCode() {
+                return time.getHour() + time.getMinute();
+            }
+
+            @Override
+            public String toString() {
+                return "Now in my " + clock + " time is: " + time;
+            }
+
+            public String getClock() {
+                return clock;
+            }
+
+            public LocalTime getTime() {
+                return time;
             }
         }
 
-        Clock clock1 = new Clock();
-        Clock clock2 = new Clock();
-        Clock clock3 = new Clock();
-        Clock clock4 = new Clock();
-        Clock clock5 = new Clock();
+        Clock clock1 = new Clock("Timex", 12, 15);
+        Clock clock2 = new Clock("Timex", 13, 29);
+        Clock clock3 = new Clock("Timex", 7, 53);
+        Clock clock4 = new Clock("Timex", 21, 19);
+        Clock clock5 = new Clock("Timex", 3, 3);
 
         List<Clock> clockList = new LinkedList<>();
         clockList.add(clock1);
@@ -23,5 +60,8 @@ class Application {
         clockList.add(clock4);
         clockList.add(clock5);
 
+        for(int i = 0; i < clockList.size(); i++) {
+            System.out.println(clockList.get(i));
+        }
     }
 }
